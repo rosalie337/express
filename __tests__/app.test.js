69830f20bebbcc48332af58e3a1a3dae4d07e0e8
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-06-09 16:06:50 -0700

    deleted id from organziation model and added routes/test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,8 +5,9 @@ const connect = require('../lib/utils/connect');
 
 const request = require('supertest');
 const app = require('../lib/app');
+const Organization = require('../lib/models/Organization')
 
-describe('express routes', () => {
+describe('organization routes', () => {
   beforeAll(async() => {
     const uri = await mongod.getUri();
     return connect(uri);
@@ -20,4 +21,17 @@ describe('express routes', () => {
     await mongoose.connection.close();
     return mongod.stop();
   });
+
+  it('creates an organization via POST', () => {
+    return request(app)
+      .post('api/v1/organizations')
+      .send({
+        name: 'Alchemy',
+        _id: 337,
+        imageUrl:  ,
+        description: 'software development school',
+      })
+  });
 });
+
+
